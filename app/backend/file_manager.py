7a8443d89cae18b35789f4caf0aa9e7d1f4753f5
sitharachari <sithara12.chari@gmail.com>
2025-10-12 23:20:04 -0700

    edited typos, and added more comments for clarity

--- app/backend/file_manager.py
+++ app/backend/file_manager.py
@@ -13,6 +13,7 @@ class FileManager:
         
         try:
             self.file_objects = []
+            #pass filepaths to helper methods
             path = self._validate_path(filepath)
             self.file_objects = self._load_files(path)
 
@@ -40,24 +41,33 @@ class FileManager:
 
     def _validate_path(self, filepath):
         # print(f"DEBUG: Raw input → {repr(filepath)}")
+
+        #remove quotations marks if user pastes file path in as input
         filepath = filepath.strip().strip('"').strip("'")
+
+        #to ensure that directory looks at paths absolutely
         path = Path(filepath).expanduser().resolve()
+
         # print(f"DEBUG: Resolved path → {path}")
         # print(f"DEBUG: Exists? {path.exists()}")
+
 
         if not path.exists():
             raise FileNotFoundError(f"Path not found: {filepath}")
         
+        #pass path to helper method to check if it is a RAR file
         if path.is_file() and self._is_rar_file(path):
-            raise ValueError(f"RAR files are not supoprted: {filepath}")
+            raise ValueError(f"RAR files are not supported: {filepath}")
         
         if path.is_file():
             size = path.stat().st_size
             if size > self.max_size_bytes:
                 size_gb = size/(1024 ** 3)
                 raise ValueError(f"File too large: {size_gb:.2f}GB (max 4GB)")
-            
+
+        #if path given is a directory    
         elif path.is_dir():
+            #helper method to get directory size
             total_size = self._get_directory_size(path)
             if total_size > self.max_size_bytes:
                 size_gb = total_size / (1024 ** 3) 
@@ -67,8 +77,9 @@ class FileManager:
 
     def _load_files(self, path):
         file_objects = [] 
-        #didn't use the file objects attribute here because I assign the result of this method to the attribue in the _load_from_filepath method anyway, and it's more explicit there.d
+        #didn't use the file objects attribute here because I assign the result of this method to the attribue in the _load_from_filepath method anyway, and it's more explicit there.
 
+        #check if file is a compressed zip file, and pass to helper method accordingly
         if path.is_file() and path.suffix.lower() == '.zip':
             file_objects = self._extract_and_load_zip(path)
         
@@ -77,6 +88,7 @@ class FileManager:
             if file_obj:
                 file_objects.append(file_obj)
         
+        #if path is a directory, look at each file within it
         elif path.is_dir():
             for file_path in path.rglob('*'):
                 if not file_path.is_file():
@@ -98,6 +110,7 @@ class FileManager:
 
     def _load_single_file(self, file_path):
         try:
+            #open files as binary so we can read binary data needed for later steps
             with open(file_path, 'rb') as f:
                 binary_data = f.read()
             return {
@@ -112,14 +125,19 @@ class FileManager:
             return None
         
 
+
     def _extract_and_load_zip(self, zip_path):
         file_objects = []
 
+        #temporary directory to store the contents of zip file we will uncompress
         self.temp_extract_dir = tempfile.mkdtemp()
         temp_path = Path(self.temp_extract_dir)
         try:
+            #use built in ZipFile library to extract contents
             with zipfile.ZipFile(zip_path, 'r') as zip_ref:
                 zip_ref.extractall(temp_path)
+
+            #run through each file we just extracted and pass those on to _load_single_file when needed    
             for file_path in temp_path.rglob('*'):
                 if not file_path.is_file():
                     continue
@@ -142,6 +160,7 @@ class FileManager:
             if self.temp_extract_dir and Path(self.temp_extract_dir).exists():
                 shutil.rmtree(self.temp_extract_dir)
 
+    #helper method to find the total size of directory
     def _get_directory_size(self, path):
         total = 0
         for file_path in path.rglob('*'):
@@ -149,10 +168,12 @@ class FileManager:
                 total += file_path.stat().st_size
         return total
     
+    #helper method to check if a file is a rar file
     def _is_rar_file(self, path):
         return path.suffix.lower() in ['.rar', '.r00', '.r01']
 
-   
+
+#to test the function of this class 
 if __name__ == "__main__":
     import os
     print(f"Current working directory: {os.getcwd()}")
