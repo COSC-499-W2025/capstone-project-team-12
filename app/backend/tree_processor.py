69e59522cc12f0f3d23407c5acab2d88e9b9cf82
Devin <devhze@yahoo.com>
2025-10-18 00:08:14 -0700

    Added a base structure for pre-order traversal

---
+++ app/backend/tree_processor.py
@@ -0,0 +1,21 @@
+from anytree import PreOrderIter
+#here we would import the getFileType function from its module
+
+def process_file_tree(root):
+    for node in PreOrderIter(root):
+
+        # Check for .git, if it is, then it marks the parent folder with the is_repo_head attribute
+        if node.name == ".git" and node.parent:
+            node.parent.is_repo_head = True
+
+        # Classify files (leaf nodes)
+        if len(node.children) == 0: # checks if it's a file or a folder
+            # Adds new attribute 'classification' to the node, the value will be the one resulting from the getFileType function
+            # getFileType is a temporary name, we will change it to the correct one when it is implemented
+            node.classification = getFileType(node.name) 
+        else:
+            node.classification = None
+            if not hasattr(node, 'is_repo_head'):
+                node.is_repo_head = False
+
+    return root
