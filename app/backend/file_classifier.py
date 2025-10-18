f2d671e55fd6507c170df1bad198d45dbe8f1908
Madelyn DeGruchy <144863481+maddydeg@users.noreply.github.com>
2025-10-18 09:46:27 -0700

    added helper methods isCode() and isText()

--- app/backend/file_classifier.py
+++ app/backend/file_classifier.py
@@ -5,3 +5,17 @@ def getFileType(file_name):
         return "text"
     else:
         return "other"
+    
+def isCode(file_name):
+    code_extensions = ['.py', '.java', '.cpp', '.js', '.rb', '.go', '.cs', '.c', '.h', '.php', '.html', '.css', '.htm']
+    for ext in code_extensions:
+        if file_name.lower().endswith(ext):
+            return True
+    return False
+
+def isText(file_name):
+    text_extensions = ['.txt', '.md', '.rtf', '.pdf', '.doc', '.docx']
+    for ext in text_extensions:
+        if file_name.lower().endswith(ext):
+            return True
+    return False
