37512e7fe54db5961f6b584e96e3ca69f75363c5
Devin <devhze@yahoo.com>
2026-01-23 12:22:18 -0800

    Fixed initdb.sq typos and logic issue

--- app/database/initdb.sql
+++ app/database/initdb.sql
@@ -4,35 +4,28 @@ SELECT uuid_generate_v4();
 
 CREATE TABLE IF NOT EXISTS
 Analyses(
-    analysis_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-    fileset_id integer REFERENCES Filesets(fileset_id),
-    data_id integer REFERENCES Tracked_Data(data_id),
-    result_id integer REFERENCES Results(result_id),
-    resume_id integer Results Resumes(resume_id),
-    portfolio_id integer REFERENCES Portfolios(portfolio_id),
-    
-    
-)
+    analysis_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY  
+);
 
 CREATE TABLE IF NOT EXISTS
 Filesets(
     fileset_id SERIAL PRIMARY KEY,
-    analysis_id integer REFERENCES Analyses(analysis_id) NOT NULL
+    analysis_id uuid REFERENCES Analyses(analysis_id) NOT NULL UNIQUE,
     file_data bytea -- Only the most recent set of binary data for the files is maintained. 
                     --But multiple trees can be maintained for the same analysis in Filetrees table
-)
+);
 
 CREATE TABLE IF NOT EXISTS
 Filetrees(
     filetree_id SERIAL PRIMARY KEY,
     fileset_id integer REFERENCES Filesets(fileset_id),
     filetree JSON
-)
+);
 
 CREATE TABLE IF NOT EXISTS 
 Tracked_Data(
-    data_id SERIAL DEFAULT PRIMARY KEY,
-    analysis_id integer REFERENCES Analyses(analysis_id) NOT NULL,
+    data_id SERIAL PRIMARY KEY,
+    analysis_id uuid REFERENCES Analyses(analysis_id) NOT NULL,
     bow_cache JSON,
     project_data JSON,
     package_data JSON,
@@ -42,7 +35,7 @@ Tracked_Data(
 CREATE TABLE IF NOT EXISTS 
 Results(
     result_id SERIAL PRIMARY KEY,
-    analysis_id integer REFERENCES Analyses(analysis_id) NOT NULL,
+    analysis_id uuid REFERENCES Analyses(analysis_id) NOT NULL,
     topic_vector JSON, --changed to JSON for more flexibility--
     resume_points JSON,
     project_insights JSON,
@@ -56,21 +49,21 @@ Results(
 CREATE TABLE IF NOT EXISTS
 Resumes(
  resume_id SERIAL PRIMARY KEY,
- analysis_id integer REFERENCES Analyses(analysis_id),
+ analysis_id uuid REFERENCES Analyses(analysis_id),
  summary JSON,
  projects JSON,
  skills JSON,
  languages JSON,
  full_resume JSON
-)
+);
 CREATE TABLE IF NOT EXISTS
 Portfolios(
     portfolio_id SERIAL PRIMARY KEY,
-    analysis_id integer REFERENCES Analyses(analysis_id)
+    analysis_id uuid REFERENCES Analyses(analysis_id)
     --TODO determine portfolio output columns
 
     
-)
+);
 
 CREATE DATABASE test_db 
 WITH TEMPLATE "user";
