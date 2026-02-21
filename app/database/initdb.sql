c03eca7dc39e9dbe82ee32b54011092219b30d20
Eveline36 <evelinesrinivasan@gmail.com>
2026-02-21 13:53:33 -0800

    minor schema edits to facilitate frontend

--- app/database/initdb.sql
+++ app/database/initdb.sql
@@ -5,31 +5,34 @@ SELECT uuid_generate_v4();
 CREATE TABLE IF NOT EXISTS
 Analyses(
     analysis_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-    original_file_path text,  -- Renamed from file_path to avoid confusion
+    analysis_title TEXT DEFAULT NULL,
+    original_file_path text,  
     creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    thumbnail_image bytea DEFAULT NULL
+    
 );
 
 CREATE TABLE IF NOT EXISTS
 Filesets(
     fileset_id SERIAL PRIMARY KEY,
-    analysis_id uuid REFERENCES Analyses(analysis_id) NOT NULL UNIQUE,
+    analysis_id uuid REFERENCES Analyses(analysis_id) NOT NULL UNIQUE ON DELETE CASCADE,
     file_data bytea, 
     file_data_tree_id integer,
-    latest_file_path text  -- Renamed from file_path 
+    latest_file_path text  
 );
 
 CREATE TABLE IF NOT EXISTS
 Filetrees(
     filetree_id SERIAL PRIMARY KEY,
-    fileset_id integer REFERENCES Filesets(fileset_id),
+    fileset_id integer REFERENCES Filesets(fileset_id)ON DELETE CASCADE,
     filetree JSON
 );
 
 CREATE TABLE IF NOT EXISTS 
 Tracked_Data(
     data_id SERIAL PRIMARY KEY,
-    analysis_id uuid REFERENCES Analyses(analysis_id) NOT NULL,
+    analysis_id uuid REFERENCES Analyses(analysis_id) NOT NULL ON DELETE CASCADE,
     bow_cache JSON,
     project_data JSON,
     package_data JSON,
@@ -39,30 +42,27 @@ Tracked_Data(
 CREATE TABLE IF NOT EXISTS 
 Results(
     result_id SERIAL PRIMARY KEY,
-    analysis_id uuid REFERENCES Analyses(analysis_id) NOT NULL,
+    analysis_id uuid REFERENCES Analyses(analysis_id) NOT NULL ON DELETE CASCADE,
     topic_vector JSON, 
     resume_points JSON,
     project_insights JSON,
     package_insights JSON,
     metadata_insights JSON,
-    thumbnail_image bytea DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS
 Resumes(
  resume_id SERIAL PRIMARY KEY,
- analysis_id uuid REFERENCES Analyses(analysis_id),
- summary JSON,
- projects JSON,
- skills JSON,
- languages JSON,
- full_resume JSON
+ analysis_id uuid REFERENCES Analyses(analysis_id) ON DELETE CASCADE,
+ resume_data JSON
 );
 
 CREATE TABLE IF NOT EXISTS
 Portfolios(
     portfolio_id SERIAL PRIMARY KEY,
-    analysis_id uuid REFERENCES Analyses(analysis_id)
+    analysis_id uuid REFERENCES Analyses(analysis_id) ON DELETE CASCADE,
+    portfolio_data JSON
+    
 );
 
 ALTER TABLE Filesets
