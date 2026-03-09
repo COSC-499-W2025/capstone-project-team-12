d30ed7e83e7cf1ed2250047a0a065f43c431e021
Madelyn DeGruchy <144863481+maddydeg@users.noreply.github.com>
2026-03-08 19:59:12 -0700

    Added resume to the flow of the system

--- app/frontend/capstone-project-frontend/src/App.tsx
+++ app/frontend/capstone-project-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Sidebar from './components/sidebar';
 import Portfolio from './pages/portfolio';
 import Onboarding from './pages/onboarding';
+import ResumeDisplay from './pages/resume_display';
 
 function App() {
   const [currentStep, setCurrentStep] = useState(1);
@@ -11,6 +12,7 @@ function App() {
       <Sidebar currentStep={currentStep} onStepChange={setCurrentStep} />
       <main style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
         {currentStep === 1 && <Onboarding onComplete={() => setCurrentStep(2)} />}
+        {currentStep === 5 && <ResumeDisplay />}
         {currentStep === 6 && <Portfolio />}
         {/* add other pages/components for other steps */}
       </main>
