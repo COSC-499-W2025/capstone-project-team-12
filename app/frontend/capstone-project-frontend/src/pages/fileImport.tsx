e431edac72285978fca3c931cfa26f320b6028d6
sitharachari <sithara12.chari@gmail.com>
2026-03-28 15:24:58 -0700

    commented out zip folder warning

--- app/frontend/capstone-project-frontend/src/pages/fileImport.tsx
+++ app/frontend/capstone-project-frontend/src/pages/fileImport.tsx
@@ -270,9 +270,9 @@ const FileImport: React.FC<FileImportProps> = ({ activeAnalysisId, onComplete, m
             <p className="text-xs text-[#9ca3af] mt-1 mb-1">
               or click to <span className="text-[#6378ff] font-semibold">browse</span>
             </p>
-             <p className="text-[10.5px] text-amber-600/80 font-bold max-w-[280px] mx-auto mt-2 leading-tight">
+             {/* <p className="text-[10.5px] text-amber-600/80 font-bold max-w-[280px] mx-auto mt-2 leading-tight">
                 Uploading a repository? Please upload it as a .zip file. Browsers strip hidden .git folders from folder uploads.
-              </p>
+              </p> */}
           </div>
 
           {/* Picker popover */}
